07b0ef25871c7ac052481ce9b4510c346f7d8f35
samira <samir.a@goldenpay.az>
2024-07-19 12:16:34 +0400

    topic config

---
+++ src/main/java/com/example/kafkaap/KafkaApplication.java
@@ -0,0 +1,24 @@
+package com.example.kafkaap;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
+@SpringBootApplication
+public class KafkaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("App started");
+    }
+}
---
+++ src/main/java/com/example/kafkaap/config/KafkaTopicConfig.java
@@ -0,0 +1,24 @@
+package com.example.kafkaap.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+}
